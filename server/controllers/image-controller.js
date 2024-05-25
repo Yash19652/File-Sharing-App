ed011e72bf8127cd21f90d9b48c8b6ee26da0883
Yash19652 <yashah76@gmail.com>
2024-05-25 23:36:24 +0530

    written upload image logic

--- server/controllers/image-controller.js
+++ server/controllers/image-controller.js
@@ -1,5 +1,23 @@
-uploadImage = () =>{
-    console.log("Uploading Image");
-}
+const File = require("../models/file");
 
-module.exports = uploadImage
+uploadImage = async (req, res) => {
+  const fileObj = {
+    path: req.file.path,
+    name: req.file.originalname,
+  };
+
+  try {
+    const file = await File.create(fileObj);
+    res.status(200).json({ path: `http://localhost:8000/file/${file._id}` });
+  } catch (error) {
+    res
+      .status(500)
+      .json({
+        message:
+          "error in uploading image to database (in upladImage function in backend)",
+        error: error.message,
+      });
+  }
+};
+
+module.exports = uploadImage;
