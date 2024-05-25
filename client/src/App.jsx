29b5a740164ef8541360b1563641b540812b0420
Yash19652 <yashah76@gmail.com>
2024-05-25 21:23:56 +0530

    added css

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,21 @@
 import React from 'react'
+import "./App.css"
 
 function App() {
   return (
-    <div>Hello World!</div>
+    <>
+    <div className="main-wrapper" >
+      <div className="container">
+        <div className="wrapper">
+          <h1>File Sharing App</h1>
+          <p>Upload And Share The Download Link</p>
+
+          <button>Upload</button>
+          <input type='file'></input>
+        </div>
+      </div>
+    </div>
+    </>
   )
 }
 
